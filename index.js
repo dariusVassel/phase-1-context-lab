985dc15c9d4800964bcfc27a1de4a90564d9c18e
+ <+>
2021-07-27 14:55:24 -0400

    Complete

--- index.js
+++ index.js
@@ -9,6 +9,80 @@
  for you to use if you need it!
  */
 
+ function createEmployeeRecord([str1, str2, str3, num]){
+    let obj = {
+        firstName : str1,
+        familyName : str2,
+        title : str3,
+        payPerHour : num,
+        timeInEvents : [],
+        timeOutEvents : []
+    }
+    return obj
+}
+
+function createEmployeeRecords(arrayOfArray){
+    let returnArr = []
+	for (const array of arrayOfArray) {
+        returnArr.push((createEmployeeRecord(array)));
+      } 
+	return returnArr
+}
+
+function createTimeInEvent(dateStamp){
+    const words = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(words[1]),
+        date: words[0]
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const words = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(words[1]),
+        date: words[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    
+	let timeOut = this.timeOutEvents.find(e =>
+		e.date === dateStamp)
+	let timeOutHour = (timeOut.hour)
+													
+    let timeIn = this.timeInEvents.find(e =>
+		e.date === dateStamp)
+	let timeInHour = (timeIn.hour)
+
+	return((timeOutHour - timeInHour)/100)
+}
+
+function wagesEarnedOnDate(dateStamp){
+	let hoursWorked = hoursWorkedOnDate.call(this, dateStamp)
+    let salary = this.payPerHour
+    return (hoursWorked * salary)
+}
+
+function calculatePayroll(employeeRecordArr){
+    let payable = employeeRecordArr.reduce((acc, employeeRecord) => {
+        return acc + allWagesFor(employeeRecord)
+    }, 0)
+
+    return payable
+}
+
+function findEmployeeByFirstName(employeeRecordArr, firstName) {
+    return employeeRecordArr.find(rec => rec.firstName === firstName)
+  }
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,4 +94,5 @@ const allWagesFor = function () {
 
     return payable
 }
+
 
